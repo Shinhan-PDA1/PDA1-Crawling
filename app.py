124164ead73a363ee650e71ecedc8189750d8754
kolion98 <45055301+kolion98@users.noreply.github.com>
2023-10-14 13:32:15 +0900

    Add API for 3 major stock data

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import requests
 import json
+import FinanceDataReader as fdr
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-def scrape_finance_data(code):
+def scrape_financial_statements_data(code):
     URL = f"https://finance.naver.com/item/main.nhn?code={code}"
     req = requests.get(URL)
 
@@ -25,17 +27,47 @@ def scrape_finance_data(code):
     a_data = json.loads(annual_json_data)
     q_data = json.loads(quater_json_data)
     total_json = {"최근 연간 실적": a_data, "최근 분기 실적": q_data}
+    
     return total_json
+
+def get_major_stock_data():
+    # 45일 전 날짜 계산
+    current_date = datetime.now();
+    delta = timedelta(days=45)
+    date_45_days_ago = current_date - delta
+    date_formatted = date_45_days_ago.strftime('%Y-%m-%d')
+
+    # KOSPI, KOSDAQ, KOSPI200 데이터 가져오기
+    kospi = fdr.DataReader('KS11', date_formatted).round(3)
+    kosdaq = fdr.DataReader('KQ11', date_formatted).round(3)
+    kospi200 = fdr.DataReader('KS200', date_formatted).round(3)
+
+    major_krxData = pd.concat([kospi['Close'], kosdaq['Close'], kospi200['Close']], axis=1)
+    major_krxData.columns = ['kospi', 'kosdaq', 'kospi200']
+    major_krxData['date'] = major_krxData.index.strftime('%Y-%m-%d')
+
+    json_result = major_krxData.to_dict(orient="records")
+    
+    return json_result
+
 
 @app.route('/api/v1/crawling', methods=['POST'])
 def crawling():
     try:
         code = request.args.get('code')
         if code:
-            result = scrape_finance_data(code)
+            result = scrape_financial_statements_data(code)
             return jsonify(result), 200
         else:
             return jsonify({"error": "종목 코드를 입력해주세요."}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 800
+
+@app.route('/api/v1/getmajorstock', methods=['POST'])
+def get_stock_data():
+    try:
+        result = get_major_stock_data()
+        return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 800
 
